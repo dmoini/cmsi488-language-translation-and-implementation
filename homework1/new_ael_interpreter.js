6d71d2a2520ca00105cc25dd8fda998f13a89de3
ianlizzo <ianliz22@icloud.com>
2019-01-29 21:37:15 -0800

    "Finished" ael interpreter exponent

---
+++ homework1/new_ael_interpreter.js
@@ -0,0 +1,57 @@
+// TODO: add to the language Ael a right-associative exponentiation operator, and write an interpreter for the extended language.
+
+const ohm = require("ohm-js");
+
+const aelGrammar = ohm.grammar(`Ael {
+  Exp     = Exp "+" Term     --plus
+          | Exp "-" Term     --minus
+          | Term
+  Term    = Term "*" Factor  --times
+          | Term "/" Factor  --divide
+          | Base
+          | Factor
+  Factor  = "-" Primary      --negate
+          | Primary
+          | Base
+  Base    = Base "**" Expo    -- exponentiation
+          | Primary
+  Expo    = Base
+          | Primary
+  Primary = "(" Exp ")"      --parens
+          | number
+  number  = digit+
+  space  := " " | "\t"
+}`);
+
+// This language is so simple, we don't need an AST.
+const semantics = aelGrammar.createSemantics().addOperation("eval", {
+  Exp_plus(e, _, t) {
+    return e.eval() + t.eval();
+  },
+  Exp_minus(e, _, t) {
+    return e.eval() - t.eval();
+  },
+  Term_times(t, _, f) {
+    return t.eval() * f.eval();
+  },
+  Term_divide(t, _, f) {
+    return t.eval() / f.eval();
+  },
+  Factor_negate(_, p) {
+    return -p.eval();
+  },
+  Primary_parens(_1, e, _2) {
+    return e.eval();
+  },
+  number(chars) {
+    return +this.sourceString;
+  },
+});
+
+const match = aelGrammar.match(process.argv[2]);
+if (match.succeeded()) {
+  console.log(semantics(match).eval());
+} else {
+  console.error(match.message);
+  process.exitCode = 1;
+}
