92c076d7905628f6b0b90cb8eeaa585f7fa1e356
ianlizzo <ianliz22@icloud.com>
2019-02-19 16:15:09 -0800

    Alexia's additions

--- homework2/regex_practice/src/regex.js
+++ homework2/regex_practice/src/regex.js
@@ -1,23 +1,27 @@
 // https://stackoverflow.com/questions/15774555/efficient-regex-for-canadian-postal-code-function
 function isCanadianPostalCode(s) {
-  return /^[^DFIOQUWZdfioquwz]\d[^DFIOQUdfioqu]\s\d[^DFIOQUdfioqu]\d$/.test(s)
+  return /^[^DFIOQUWZdfioquwz]\d[^DFIOQUdfioqu]\s\d[^DFIOQUdfioqu]\d$/.test(s);
 }
 
 // https://www.regular-expressions.info/creditcard.html
 function isLegalVisaCard(s) {
-  return /^4[0-9]{12}(?:[0-9]{3}){1,2}?$/.test(s)
+  return /^4[0-9]{12}(?:[0-9]{3}){1,2}?$/.test(s);
 }
 
 // https://www.regular-expressions.info/creditcard.html
 function isLegalMasterCard(s) {
-  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(s)
+  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(
+    s
+  );
 }
 
 function isAdaNumericLiteral(s) {
-  const decimal_literal = new RegExp(/[0-9_]*\.?[0-9_]+([eE][+-]?[0-9_]+)?/)
-  const based_literal = new RegExp(/(([2-9]|1[0-6])#)([0-9A-F_]*\.?[0-9A-F_]+)+(#[eE][+-]?[0-9_]+)/)
-  const numeric_literal = new RegExp(decimal_literal + "|" + based_literal)
-  return numeric_literal.test(s)
+  const decimal_literal = new RegExp(/[0-9_]*\.?[0-9_]+([eE][+-]?[0-9_]+)?/);
+  const based_literal = new RegExp(
+    /(([2-9]|1[0-6])#)([0-9A-F_]*\.?[0-9A-F_]+)+(#[eE][+-]?[0-9_]+)/
+  );
+  const numeric_literal = new RegExp(decimal_literal + "|" + based_literal);
+  return numeric_literal.test(s);
 }
 
 // Also we should save some for Serena and Teddy
@@ -26,30 +30,27 @@ function isAdaNumericLiteral(s) {
 
 // Not sure about the correctness of this syntax
 function isBasicLatin1(s) {
-  return /^[a-zA-Z][^oO][^oO][a-zA-Z]*$/.test(s)
+  return /^[a-zA-Z][^oO][^oO][a-zA-Z]*$/.test(s);
+  // Doesn't work for single-letter or two-letter words.
 }
 
-// Hey I'll do this one! -Alexia
 function isBinaryDivisibleByThirtyTwo(s) {
-  // make sure the last 5 digits are '00000' -Donovan
-  return 
+  return /^0{1,4}$|(1*0*)*00000$/.test(s);
 }
 
 function isDecimalTwoToThirtySix(s) {
-  return /^[2-9]|[1-2][0-9]|3[0-6]$/.test(s)
+  return /^[2-9]|[1-2][0-9]|3[0-6]$/.test(s);
 }
 
 function isNonNestingMLStyle(s) {
-  return
+  return /^\(\*[a-zA-Z]*\*\)$/.test(s);
 }
 
 // I'll try and do this one too :) - Alexia
 function isBasicLatin2(s) {
-  return
+  return /^[a-z]*[^file][^find][^for]$/.test(s);
 }
 
 function isBasicLatin3(s) {
-  return
+  return;
 }
-
-
