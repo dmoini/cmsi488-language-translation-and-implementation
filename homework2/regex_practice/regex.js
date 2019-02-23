ec6b17083ca74506371d40a6af3ab693487ac0fb
ianlizzo <ianliz22@icloud.com>
2019-02-22 22:21:46 -0800

    Got tests to work, need to clean up

--- homework2/regex_practice/regex.js
+++ homework2/regex_practice/regex.js
@@ -4,58 +4,84 @@ function isCanadianPostalCode(s) {
 }
 
 // https://www.regular-expressions.info/creditcard.html
-function isLegalVisaCard(s) {
+function isVisa(s) {
   return /^4[0-9]{12}(?:[0-9]{3}){1,2}?$/.test(s);
 }
 
 // https://www.regular-expressions.info/creditcard.html
-function isLegalMasterCard(s) {
-  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(s);
+function isMasterCard(s) {
+  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(
+    s
+  );
 }
 
-function isAdaNumericLiteral(s) {
-  const decimal_literal = new RegExp(
-    /^[0-9]+(\_[0-9]+)*(\.([0-9]+(\_[0-9]+)*))?([eE][+-]?[0-9]+(\_[0-9]+)*)?$/
+function isAdaFloat(s) {
+  const decimal_literal = /^[0-9]+(\_[0-9]+)*(\.([0-9]+(\_[0-9]+)*))?([eE][+-]?[0-9]+(\_[0-9]+)*)?$/;
+
+  const based_literal = /^([2-9]|1[0-6])#[0-9A-Fa-f]+(\_[0-9A-Fa-f]+)*(\.([0-9A-Fa-f]+(\_[0-9A-Fa-f]+)*))?#([eE][+-]?[0-9]+(\_[0-9]+)*)?$/;
+
+  const numeric_literal = new RegExp(
+    "(" + based_literal.source + ")|(" + decimal_literal.source + ")"
   );
-  const based_literal = new RegExp(
-    /^([2-9]|1[0-6])#[0-9]+(\_[0-9A-Fa-f]+)*(\.([0-9A-Fa-f]+(\_[0-9A-Fa-f]+)*))?#([eE][+-]?[0-9]+(\_[0-9]+)*)?$/
-  );
-  const numeric_literal = new RegExp(decimal_literal + "|" + based_literal);
   return numeric_literal.test(s);
 }
 
 // https://regex101.com
 
 // Checked! Works.
-function isBasicLatin1(s) {
+function isNotThreeEndingInOO(s) {
   return /^([a-nA-N]*[p-zP-Z]*)*[oO]?([a-nA-N]*[p-zP-Z]*)*$/.test(s);
 }
 
 // Checked! Works.
-function isBinaryDivisibleByThirtyTwo(s) {
+function isDivisibleBy32(s) {
   return /^0{1,4}$|(1*0*)*00000$/.test(s);
 }
 
 // Checked! Works.
-function isDecimalTwoToThirtySix(s) {
+function isTwoThroughThirtySix(s) {
   return /^\b([2-9]|[12][0-9]|3[0-6])\b$/.test(s);
 }
 
-function isNonNestingMLStyle(s) {
+function isMLComment(s) {
   return /^\(\*[a-zA-Z]*\*\)$/.test(s);
 }
 
 // Works! Full expression: ^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$|^fin[^d][a-zA-Z]*$|^find[a-zA-Z]+$|^fo[^r][a-zA-Z]*$|^for[a-zA-Z]+$|^[^f]+[a-zA-Z]*$|^f$|^fo$|^fi$|^fin$|^fil$
-function isBasicLatin2(s) {
+function isNotForFileFindNoLookAround(s) {
   const substrings = new RegExp(/^f$|^fo$|^fi$|^fin$|^fil$/);
-  const not_file = new RegExp(/^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$/);
+  const not_file = new RegExp(
+    /^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$/
+  );
   const not_find = new RegExp(/^fin[^d][a-zA-Z]*$|^find[a-zA-Z]+$/);
   const not_for = new RegExp(/^fo[^r][a-zA-Z]*$|^for[a-zA-Z]+$/);
   const not_start_with_f = new RegExp(/^[^f]+[a-zA-Z]*$/);
-  const basic_latin = new RegExp(substrings + "|" + not_file + "|" + not_find + "|" + not_for + "|" + not_start_with_f);
+  const basic_latin = new RegExp(
+    substrings +
+      "|" +
+      not_file +
+      "|" +
+      not_find +
+      "|" +
+      not_for +
+      "|" +
+      not_start_with_f
+  );
   return basic_latin.test(s);
 }
 
 function isBasicLatin3(s) {
   return /\b(?:[a-eg-z]|f(?!ile\b)(?!ind\b)(?!or))\w*\b/.test(s);
 }
+
+module.exports = {
+  isCanadianPostalCode,
+  isVisa,
+  isMasterCard,
+  isAdaFloat,
+  isNotThreeEndingInOO,
+  isDivisibleBy32,
+  isTwoThroughThirtySix,
+  isMLComment,
+  isNotForFileFindNoLookAround,
+};
---
+++ homework2/regex_practice/test/test.js
@@ -0,0 +1,63 @@
+const assert = require("assert");
+const r = require("../regex");
+
+const FIXTURE = {
+  isCanadianPostalCode: {
+    good: ["A7X 2P8", "P8E 4R2"],
+    bad: ["A7X   9B2", "C7E9U2", "", "Dog"],
+  },
+  isVisa: {
+    good: ["4128976567772613", "4089655522138888"],
+    bad: ["43333", "42346238746283746823"],
+  },
+  isMasterCard: {
+    good: ["5100000000000000", "5294837679998888"],
+    bad: ["5763777373890002", "513988843211541", "5432333xxxxxxxxx"],
+  },
+  isAdaFloat: {
+    good: ["1", "23_5", "4#20#", "13#fD34_2_1#", "1.3e2", "11_3.3_3_222E-199"],
+    bad: ["dog", "4fe", "p#ii#", "_", "_33", "5__2", "9#88#E-1e3", "-6"],
+  },
+  isNotThreeEndingInOO: {
+    good: ["", "fog", "Tho"],
+    bad: ["fOo", "gOO"],
+  },
+  isDivisibleBy32: {
+    good: ["0", "110100000"],
+    bad: ["1", "0000000010000", "1000000001"],
+  },
+  isTwoThroughThirtySix: {
+    good: Array(35)
+      .fill(0)
+      .map((x, i) => i + 2),
+    bad: ["1", "0", "00003", "dog", "361"],
+  },
+  isMLComment: {
+    good: ["(**)", "(*  *)", "(*756****)"],
+    bad: ["", "(*)", "(* before (* inner *) after *)"],
+  },
+  isNotForFileFindNoLookAround: {
+    good: ["", "files", "fors", "dog"],
+    bad: ["file", "find"],
+  },
+};
+
+// Looks funny, but you can probably figure out what it does
+FIXTURE.isNotForFileFindWithLookAround = FIXTURE.isNotForFileFindNoLookAround;
+
+describe("In the regex tester", () => {
+  Object.entries(r).forEach(([name, matchingFunction]) => {
+    describe(`the function ${name}`, () => {
+      FIXTURE[name].good.forEach(s => {
+        it(`accepts ${s}`, () => {
+          assert.ok(matchingFunction(s));
+        });
+      });
+      FIXTURE[name].bad.forEach(s => {
+        it(`rejects ${s}`, () => {
+          assert.ok(!matchingFunction(s));
+        });
+      });
+    });
+  });
+});
