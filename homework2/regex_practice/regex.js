270ea2598105b97742c76bf02f86e0c6faa41978
Donovan Moini <donovanmoini@gmail.com>
2019-02-23 16:28:20 -0800

    Spacing formatting

--- homework2/regex_practice/regex.js
+++ homework2/regex_practice/regex.js
@@ -10,19 +10,13 @@ function isVisa(s) {
 
 // https://www.regular-expressions.info/creditcard.html
 function isMasterCard(s) {
-  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(
-    s
-  );
+  return /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/.test(s);
 }
 
 function isAdaFloat(s) {
   const decimal_literal = /^[0-9]+(\_[0-9]+)*(\.([0-9]+(\_[0-9]+)*))?([eE][+-]?[0-9]+(\_[0-9]+)*)?$/;
-
   const based_literal = /^([2-9]|1[0-6])#[0-9A-Fa-f]+(\_[0-9A-Fa-f]+)*(\.([0-9A-Fa-f]+(\_[0-9A-Fa-f]+)*))?#([eE][+-]?[0-9]+(\_[0-9]+)*)?$/;
-
-  const numeric_literal = new RegExp(
-    "(" + based_literal.source + ")|(" + decimal_literal.source + ")"
-  );
+  const numeric_literal = new RegExp("(" + based_literal.source + ")|(" + decimal_literal.source + ")");
   return numeric_literal.test(s);
 }
 
@@ -43,11 +37,10 @@ function isMLComment(s) {
 }
 
 // Works! Full expression: ^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$|^fin[^d][a-zA-Z]*$|^find[a-zA-Z]+$|^fo[^r][a-zA-Z]*$|^for[a-zA-Z]+$|^[^f]+[a-zA-Z]*$|^f$|^fo$|^fi$|^fin$|^fil$
+// TODO: fix
 function isNotForFileFindNoLookAround(s) {
   const substrings = new RegExp(/^f$|^fo$|^fi$|^fin$|^fil$/);
-  const not_file = new RegExp(
-    /^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$/
-  );
+  const not_file = new RegExp(/^f[^io][a-zA-Z]*$|^fi[^ln][a-zA-Z]*$|^fil[^e][a-zA-Z]*$|^file[a-zA-Z]+$/);
   const not_find = new RegExp(/^fin[^d][a-zA-Z]*$|^find[a-zA-Z]+$/);
   const not_for = new RegExp(/^fo[^r][a-zA-Z]*$|^for[a-zA-Z]+$/);
   const not_start_with_f = new RegExp(/^[^f]+[a-zA-Z]*$/);
@@ -55,6 +48,7 @@ function isNotForFileFindNoLookAround(s) {
   return basic_latin.test(s);
 }
 
+// TODO: fix
 function isNotForFileFindWithLookAround(s) {
   return /\b(?:[a-eg-z]|f(?!ile\b)(?!ind\b)(?!or))\w*\b/.test(s);
 }
