4c70ed982fe176f45e6a5c0228ee8ba6022ed032
Donovan Moini <donovanmoini@gmail.com>
2019-02-25 00:35:25 -0800

    Finished DivisibleBy32

--- homework2/ohm_practice/ohm.js
+++ homework2/ohm_practice/ohm.js
@@ -2,7 +2,7 @@ const ohm = require("ohm-js");
 
 const GRAMMARS = {
   CanadianPostalCode: `CanadianPostalCode {
-    exp               = firstHalf space secondHalf          --code
+    exp               = firstHalf space secondHalf              --code
     firstHalf         = firstAlpha digit goodAlpha
     secondHalf        = digit goodAlpha digit
     firstAlpha        = ~badFirst upper
@@ -21,10 +21,10 @@ const GRAMMARS = {
     exp               = firstDigits lastDigits
     firstDigits       = caseA | caseB
     caseA             = "5" "1".."5" digit digit          
-    caseB             = "222" digit                         --rangeOne
-                      | "22" "3".."9" digit                 --rangeTwo
-                      | "2" "3".."6" digit digit            --rangeThree
-                      | "27" "0".."1" digit                 --rangeFour
+    caseB             = "222" digit                             --rangeOne
+                      | "22" "3".."9" digit                     --rangeTwo
+                      | "2" "3".."6" digit digit                --rangeThree
+                      | "27" "0".."1" digit                     --rangeFour
                       | "2720"                         
     lastDigits        = fourDigits fourDigits fourDigits 
     fourDigits        = digit digit digit digit
@@ -39,45 +39,41 @@ const GRAMMARS = {
     exponent          = ("e" | "E") ("+" | "-")? numeral
     basenum			      = hexDigit+ ("_" hexDigit+)* 			
     numeral           = digit+ ("_" digit+)*					
-    base 				      = ("1".."2") digit     					      --tensAndTwenties
-                      | "3" "0".."2"       				          --thirties
-                      | "2".."9"							              --twoToNine
+    base 				      = ("1".."2") digit     					          --tensAndTwenties
+                      | "3" "0".."2"       				              --thirties
+                      | "2".."9"							                  --twoToNine
   }`,
   
   NotThreeEndingInOO: `NotThreeEndingInOO {
-    exp               = (overThreeString | threeString |  oneTwoStrings)?
+    exp               = (overThreeString | threeString | oneTwoStrings)?
     oneTwoStrings     = letter letter?
     threeString       = letter ~(caseInsensitive<"oo">) letter letter 
     overThreeString   = letter letter letter letter+
   }`,
   
-  // TODO
   DivisibleBy32: `DivisibleBy32 {
-    exp               = oneToFourZeroes 
-                      | zeroOrOne* fiveZeroes               --multipleZeroes
-    oneToFourZeroes   = "0" "0"? "0"? "0"?
-    zeroOrOne         = "0" | "1"
-    fiveZeroes        = "00000"
-  }`,
+    exp          	    = (~("00000" end) ("0" | "1"))* "00000"		--nonZero
+    				          | "0" "0"? "0"? "0"?				              --zero
+}`,
   
   TwoThroughThirtySix: `TwoThroughThirtySix {
-    number            = "1".."2" "0".."9"                   --tensAndTwenties
-                      | "3" "0".."6"                        --thirties
+    number            = "1".."2" "0".."9"                       --tensAndTwenties
+                      | "3" "0".."6"                            --thirties
                       | "2".."9"
   }`,
   
   // TODO
   MLComment: `MLComment {
     comment           = "(*" exp "*)" 
-    exp               = ~comment                            --notComment
-                      | any*                                --any
+    exp               = ~comment                                --notComment
+                      | any*                                    --any
   }`,
   
   // TODO
   NotForFileFindNoLookAround: `NotForFileFindNoLookAround {
-    exp               = noFile                              --noFile
-                      | noFor                               --noFor
-                      | noFind                              --noFind
+    exp               = noFile                                  --noFile
+                      | noFor                                   --noFor
+                      | noFind                                  --noFind
                       | other
     noFile            = "fil" ("a".."d" | "f" .. "z") | "file" letter+ | letter+ "file"
     noFor             = "fo" ("a".."q" | "s".."z") | "for" letter+ | letter+ "for"
